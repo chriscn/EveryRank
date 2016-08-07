ec0924972e3d165382732f4e13da36a8ec821830
ThorinDev <c.nethercott@icloud.com>
2016-08-07 17:06:03 +0100

    Added the first part of the message

--- src/com/thorindev/EveryRank.java
+++ src/com/thorindev/EveryRank.java
@@ -21,6 +21,25 @@ public class EveryRank extends JavaPlugin {
 	// <command> <message>
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player player = (Player) sender;
+		
+		if(cmd.getName().equalsIgnoreCase("everyrank")) {
+			if(args.length == 0) {
+				player.sendMessage(ChatColor.GRAY + player.getDisplayName());
+				player.sendMessage(ChatColor.GREEN + "[VIP] "+ player.getDisplayName());
+				player.sendMessage(ChatColor.GREEN + "[VIP" + ChatColor.GOLD + "+" + ChatColor.GREEN + "] " + player.getDisplayName());
+				player.sendMessage(ChatColor.AQUA + "[MVP] " + player.getDisplayName());
+				player.sendMessage(ChatColor.AQUA + "[MVP" + ChatColor.RED + ChatColor.RED + "+" + ChatColor.AQUA + "] " + player.getDisplayName());
+				player.sendMessage(ChatColor.BLUE + "[JR HELPER] " + player.getDisplayName());
+				player.sendMessage(ChatColor.BLUE + "[HELPER] " + player.getDisplayName());
+				player.sendMessage(ChatColor.DARK_GREEN + "[MOD] " + player.getDisplayName());
+				player.sendMessage(ChatColor.RED + "[ADMIN] " + player.getDisplayName());
+				player.sendMessage(ChatColor.RED + "[OWNER] " + player.getDisplayName());
+				player.sendMessage(ChatColor.RED + "[SLOTH] " + player.getDisplayName());
+				player.sendMessage(ChatColor.DARK_AQUA + "[BUILD TEAM] " + player.getDisplayName());
+				player.sendMessage(ChatColor.DARK_AQUA + "[BUILD TEAM" + ChatColor.RED + "+] " + ChatColor.DARK_AQUA + player.getDisplayName());
+				player.sendMessage(ChatColor.GOLD + "[MOJANG] " + player.getDisplayName());
+				player.sendMessage(ChatColor.GOLD + "[YT]" + player.getDisplayName());
+			}
 		return false;
 	}
 }
