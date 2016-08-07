f9ce1d5fa10debceba559ae2a92d7434d3cec3c6
ThorinDev <c.nethercott@icloud.com>
2016-08-07 17:05:32 +0100

    Added the onCommand boolean

--- src/com/thorindev/EveryRank.java
+++ src/com/thorindev/EveryRank.java
@@ -17,4 +17,8 @@ public class EveryRank extends JavaPlugin {
 	public void onDisable() {
 		getLogger().info("Disabling EveryRank");
 	}
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		return false;
+	}
 }
