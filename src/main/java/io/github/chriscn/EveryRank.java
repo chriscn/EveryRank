c726aadaf2526655e6b7b0afc5a289a08582cc97
Chris Nethercott <ccnethercott@gmail.com>
2017-08-13 14:42:39 +0100

    Copied pasted ready

--- src/main/java/io/github/chriscn/EveryRank.java
+++ src/main/java/io/github/chriscn/EveryRank.java
@@ -53,6 +53,26 @@ public class EveryRank extends JavaPlugin {
                     return true;
                 } else if(args.length >= 1) {
                     String msg = buildString(args);
+                    p.sendMessage(ChatColor.GRAY + pN +);
+                    p.sendMessage(ChatColor.GREEN + "[VIP] " + pN);
+                    p.sendMessage(ChatColor.GREEN + "[VIP" + ChatColor.GOLD + "+" + ChatColor.GREEN + "] " + pN);
+                    p.sendMessage(ChatColor.GREEN + "[LOL] " + pN);
+                    p.sendMessage(ChatColor.GREEN + "[LOL" + ChatColor.GOLD + "+" + ChatColor.GREEN + "] " + pN);
+                    p.sendMessage(ChatColor.AQUA + "[MVP] " + pN);
+                    p.sendMessage(ChatColor.AQUA + "[MVP" + ChatColor.RED + ChatColor.RED + "+" + ChatColor.AQUA + "] " + pN);
+                    p.sendMessage(ChatColor.BLUE + "[JR HELPER] " + pN);
+                    p.sendMessage(ChatColor.BLUE + "[HELPER] " + pN);
+                    p.sendMessage(ChatColor.DARK_GREEN + "[MOD] " + pN);
+                    p.sendMessage(ChatColor.RED + "[ADMIN] " + pN);
+                    p.sendMessage(ChatColor.RED + "[OWNER] " + pN);
+                    p.sendMessage(ChatColor.RED + "[SLOTH] " + pN);
+                    p.sendMessage(ChatColor.RED + "[ANGUS] " + pN);
+                    p.sendMessage(ChatColor.DARK_AQUA + "[BUILD TEAM] " + pN);
+                    p.sendMessage(ChatColor.DARK_AQUA + "[BUILD TEAM" + ChatColor.RED + "+" + ChatColor.DARK_AQUA + "] " + pN);
+                    p.sendMessage(ChatColor.GOLD + "[MOJANG] " + pN);
+                    p.sendMessage(ChatColor.GOLD + "[YT] " + pN);
+                    p.sendMessage(ChatColor.GOLD + "[APPLE] " + pN);
+                    p.sendMessage(ChatColor.RED + "[" + ChatColor.GREEN + "MC" + ChatColor.GRAY + "ProHosting" + ChatColor.RED + "] " + pN);
                 }
             }
 
