5b9f7e0de12f8b7fb25ead1d0d7046c93f2b73a0
Chris Nethercott <ccnethercott@gmail.com>
2017-08-13 14:31:39 +0100

    Checking what command was fired

--- src/main/java/io/github/chriscn/EveryRank.java
+++ src/main/java/io/github/chriscn/EveryRank.java
@@ -1,5 +1,9 @@
 package io.github.chriscn;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EveryRank extends JavaPlugin {
@@ -13,6 +17,13 @@ public class EveryRank extends JavaPlugin {
     public void onDisable() {
         getLogger().info(getDescription().getName() + " has been disabled!");
 
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("everyrank")) {
+
+        }
     }
 
 }
