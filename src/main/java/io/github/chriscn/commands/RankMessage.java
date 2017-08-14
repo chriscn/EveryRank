aa7902e20e17b42fd9278930f70d53ad8c79b489
Chris Nethercott <ccnethercott@gmail.com>
2017-08-14 12:01:33 +0100

    this took too long

--- src/main/java/io/github/chriscn/commands/RankMessage.java
+++ src/main/java/io/github/chriscn/commands/RankMessage.java
@@ -1,5 +1,6 @@
 package io.github.chriscn.commands;
 
+import io.github.chriscn.RANKS;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -13,7 +14,85 @@ public class RankMessage implements CommandExecutor {
             return true;
         } else {
             Player p = (Player) sender;
-            String pN = args[0];
+            String rank = args[0].toLowerCase();
+            String pN = args[1];
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = 2; i < args.length; i++) {
+                sb.append(i).append(" ");
+            }
+
+            String msg = sb.toString().trim();
+
+            switch(rank) {
+                default:
+                    printHelp(p);
+                    break;
+                case "":
+                    printHelp(p);
+                    break;
+                case "default":
+                    p.sendMessage(ChatColor.GRAY + pN + ": " + msg);
+                    break;
+                case "vip":
+                    printMessage(p, pN, RANKS.VIP, msg);
+                    break;
+                case "vip_plus":
+                    printMessage(p, pN, RANKS.VIP_PLUS, msg);
+                    break;
+                case "lol":
+                    printMessage(p, pN, RANKS.LOL, msg);
+                    break;
+                case "lol_plus":
+                    printMessage(p, pN, RANKS.LOL_PLUS, msg);
+                    break;
+                case "mvp":
+                    printMessage(p, pN, RANKS.MVP, msg);
+                    break;
+                case "mvp_plus":
+                    printMessage(p, pN, RANKS.MVP_PLUS, msg);
+                    break;
+                case "jrhelper":
+                    printMessage(p, pN, RANKS.JR_HELPER, msg);
+                    break;
+                case "helper":
+                    printMessage(p, pN, RANKS.HELPER, msg);
+                    break;
+                case "mod":
+                    printMessage(p, pN, RANKS.MOD, msg);
+                    break;
+                case "admin":
+                    printMessage(p, pN, RANKS.ADMIN, msg);
+                    break;
+                case "owner":
+                    printMessage(p, pN, RANKS.OWNER, msg);
+                    break;
+                case "sloth":
+                    printMessage(p, pN, RANKS.SLOTH, msg);
+                    break;
+                case "angus":
+                    printMessage(p, pN, RANKS.ANGUS, msg);
+                    break;
+                case "buildteam":
+                    printMessage(p, pN, RANKS.BUILD_TEAM, msg);
+                    break;
+                case "buildteam_plus":
+                    printMessage(p, pN, RANKS.BUILD_TEAM_PLUS, msg);
+                    break;
+                case "mojang":
+                    printMessage(p, pN, RANKS.MOJANG, msg);
+                    break;
+                case "youtube":
+                    printMessage(p, pN, RANKS.YOUTUBE, msg);
+                    break;
+                case "apple":
+                    printMessage(p, pN, RANKS.APPLE, msg);
+                    break;
+                case "mcprohosting":
+                    printMessage(p, pN, RANKS.MCPROHOSTING, msg);
+                    break;
+            }
+
             //rankmessage <RANK> <PLAYERNAME> <MESSAGE>
 
 
@@ -22,4 +101,14 @@ public class RankMessage implements CommandExecutor {
         }
     }
 
+
+    private void printMessage(Player p, String pN, RANKS rank, String msg) {
+        p.sendMessage(rank.getRank() + pN + ChatColor.WHITE + ": " + msg);
+    }
+
+    private void printHelp(Player p) {
+        p.sendMessage(ChatColor.RED + "I couldn't find the rank that you wanted the please use one of these instead.");
+        p.sendMessage(ChatColor.GREEN + "The availiable ranks are:");
+        p.sendMessage(ChatColor.GREEN + "default, vip, vip_plus, lol, lol_plus, mvp, mvp_plus, jrhelper, helper, mod, admin, owner, sloth, angus, buildteam, buildteam_plus, mojang, youtube, apple, mcprohosting");
+    }
 }
